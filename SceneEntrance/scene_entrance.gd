125c00e3dfd03bb84b1c739c5a23c8a54ddcaabf
daeldevs <daeldevs@gmail.com>
2026-01-10 00:21:10 -0500

    adding DungeonScene and its respective walls and floors tilemaplayer

--- SceneEntrance/scene_entrance.gd
+++ SceneEntrance/scene_entrance.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var next_scene: String
+@export var player_spawn_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,10 +15,11 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body):
 	if body is Player:
-		print("the player has entered!")
+		SceneManager.player_spawn_position = player_spawn_position
+		#print("the player has entered!")
 		get_tree().change_scene_to_file.call_deferred(next_scene)
 
 
 func _on_body_exited(body: Node2D) -> void:
-	print("the player has exited!")
+	#print("the player has exited!")
 	pass # Replace with function body.
