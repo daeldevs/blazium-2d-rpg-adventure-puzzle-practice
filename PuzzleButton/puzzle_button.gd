553f37887bc85644470567890265eebad3a58c0d
daeldevs <daeldevs@gmail.com>
2026-01-12 16:08:10 -0500

    creating puzzle button 2

--- PuzzleButton/puzzle_button.gd
+++ PuzzleButton/puzzle_button.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var bodies_on_top: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,13 +13,17 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
+	bodies_on_top += 1	
 	if body.is_in_group("pushable") or body is Player:
-		print("I have been pushed")
-		$AnimatedSprite2D.play("pressed")
+		if bodies_on_top == 1:
+			print("I have been pushed")
+			$AnimatedSprite2D.play("pressed")
 	
 
 
 func _on_body_exited(body: Node2D) -> void:
+	bodies_on_top -= 1
 	if body.is_in_group("pushable") or  body is Player:
-		print ("i'm no longer pushed!")
-		$AnimatedSprite2D.play("unpressed")
+		if bodies_on_top == 0:
+			print ("i'm no longer pushed!")
+			$AnimatedSprite2D.play("unpressed")
