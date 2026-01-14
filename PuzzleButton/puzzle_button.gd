2f2e70b9f3a8893b0310ddb7cdfc2c240732bcb5
Staff IT <daeldevs@gmail.com>
2026-01-14 09:12:08 +0700

    creating inherited scene ice block and single use puzzle on dungeon scene

--- PuzzleButton/puzzle_button.gd
+++ PuzzleButton/puzzle_button.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var is_single_use: bool = false
+
 var bodies_on_top: int = 0
 
 signal pressed
@@ -26,6 +28,8 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_body_exited(body: Node2D) -> void:
+	if is_single_use:
+		return 
 	bodies_on_top -= 1
 	if body.is_in_group("pushable") or  body is Player:
 		if bodies_on_top == 0:
