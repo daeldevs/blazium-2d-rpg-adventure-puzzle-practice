2d33377e90c6e32102ff39d459c0eef36c434152
daeldevs <daeldevs@gmail.com>
2026-01-18 13:34:42 +0700

    testing array on npc script

--- NPC/npc.gd
+++ NPC/npc.gd
@@ -1,10 +1,18 @@
 extends StaticBody2D
 
+var dialogue_lines: Array[String] = ["hi!", "How are you?", "Bye!"]
+
 var can_interact: bool = false 
+
+func _ready():
+	print(dialogue_lines[0])
+	print(dialogue_lines[2])
 
 func _process(delta):
 	if Input.is_action_just_pressed("interact") and can_interact :
 		if $CanvasLayer.visible:
 			$CanvasLayer.visible = false
+			get_tree().paused = false
 		else:
 			$CanvasLayer.visible = true
+			get_tree().paused = true
