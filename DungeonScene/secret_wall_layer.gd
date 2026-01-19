ffe19497944f70c601be6ffd320c6e7fab6d9756
Staff IT <daeldevs@gmail.com>
2026-01-19 15:10:07 +0700

    adding secret wall

---
+++ DungeonScene/secret_wall_layer.gd
@@ -0,0 +1,13 @@
+extends TileMapLayer
+
+
+
+
+func disable_secret_wall():
+	visible = false
+	collision_enabled = false
+	
+
+func enable_secret_wall():
+	visible = true
+	collision_enabled = true
--- Switch/switch.gd
+++ Switch/switch.gd
@@ -9,18 +9,19 @@ var is_activated: bool = false
 func _process(delta):
 	if Input.is_action_just_pressed("interact") and can_interact:
 		if is_activated:
-			$AnimatedSprite2D.play("deactivated")
-			switch_deactivated.emit()
-			is_activated = false
+			deactivate_switch()
 		else:
-			$AnimatedSprite2D.play("activated")
-			switch_activated.emit()
-			is_activated = true
+			activate_switch()
+			
 
+func activate_switch():
+	$AnimatedSprite2D.play("activated")
+	switch_activated.emit()
+	is_activated = true
 
-func _on_switch_activated():
-	print("the switch was activated using a signal")
-
-
-func _on_switch_deactivated():
-	print ("the switch was deactivated using a signal")
+func deactivate_switch():
+	$AnimatedSprite2D.play("deactivated")
+	switch_deactivated.emit()
+	is_activated = false
+	
+	
