71a9e38d4afadf64f9c4b5d4cf80bea31b1d3615
Staff IT <daeldevs@gmail.com>
2026-01-19 15:54:21 +0700

    adding switch puzzles with switchpuzzlemanager

---
+++ Switch/switch_puzzle_manager.gd
@@ -0,0 +1,19 @@
+extends Marker2D
+
+signal puzzle_solved
+signal puzzle_failed
+
+@export var target_score: int 
+var score:int
+
+func increase_score():
+	score += 1
+	print (score)
+	if score >= target_score:
+		puzzle_solved.emit()
+
+func decrease_score():
+	score -= 1
+	print (score)
+	if score < target_score:
+		puzzle_failed.emit()
