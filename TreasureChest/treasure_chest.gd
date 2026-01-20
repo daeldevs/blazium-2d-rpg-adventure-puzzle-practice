ddda081831cac45a4328f748e5e13f5c40032023
Staff IT <daeldevs@gmail.com>
2026-01-20 13:51:15 +0700

    set up remembering opened chests

--- TreasureChest/treasure_chest.gd
+++ TreasureChest/treasure_chest.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 var can_interact: bool = false
 var is_open: bool = false
-
+@export var chest_name: String 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -11,11 +11,19 @@ func _process(delta):
 			open_chest()
 
 
+
+func _ready():
+	if SceneManager.opened_chests.has(chest_name):
+		is_open = true
+		$AnimatedSprite2D.play("open")
+
 func open_chest():
 	is_open = true
 	$AnimatedSprite2D.play("open")
 	$Sprite2D.visible = true
 	$Timer.start()
+	SceneManager.opened_chests.append(chest_name)
+	print(SceneManager.opened_chests)
 
 
 func _on_timer_timeout():
