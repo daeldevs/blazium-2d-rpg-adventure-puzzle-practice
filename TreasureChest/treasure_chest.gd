f342c2bc636e03f87eca09000b64e71919c95594
Staff IT <daeldevs@gmail.com>
2026-01-20 08:38:16 +0700

    adding treasure chest on dungeon scene

---
+++ TreasureChest/treasure_chest.gd
@@ -0,0 +1,22 @@
+extends StaticBody2D
+
+var can_interact: bool = false
+var is_open: bool = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("interact") and can_interact:
+		if not is_open:
+			open_chest()
+
+
+func open_chest():
+	is_open = true
+	$AnimatedSprite2D.play("open")
+	$Sprite2D.visible = true
+	$Timer.start()
+
+
+func _on_timer_timeout():
+	$Sprite2D.visible = false
